b7a081808d401dff9ff032cae52b747387471845
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-02-28 04:47:40 +1300

    updated logo files and added new photo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Tilt from 'react-parallax-tilt';
+import brain from './brain.jpg';
 import './Logo.css';
 
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
-            <Tilt className='Tilt br2 shadow-2' options={{ max: 25 }} style={{ height: '150px', width: '150px'}}>
-                <h1> 👀</h1>
+            <Tilt className='Tilt br2 shadow-2' options={{ max: 55 }} style={{ height: '250px', width: '250px'}}>
+                <h1><img style={{padding:'10px 0px 10px 0px'}} src={brain} alt='logo'/></h1>
             </Tilt>
         </div>
     );
