3d867f153818f150309b608c87162c05acb8d2d5
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-02-28 22:06:28 +1300

    added components and logo folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Navigation />
+      {/* {<Logo />
+              <ImageLinkForm />
+              <FaceRecognition />} */}
     </div>
   );
 }
