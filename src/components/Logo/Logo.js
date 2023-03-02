4d7e6ef52fa1a01cfe0df7266799a5a0305a20d1
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-03-02 22:08:27 +1300

    added logo files

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,2 +1,18 @@
-import React from "react";
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import './Logo.css';
 
+
+const Logo = () => {
+    return (
+        <div className='pa3 mt0'>
+            <Tilt className='Tilt br2 shadow-2'>
+                <div options={{ max : 25 }} style={{ width: '50px', height: '150px' }}>
+                    <h1 className='Tilt-inner'>React Parallax Tilt 👀</h1>
+                </div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
