f83289f1df428549c4d6cf79481896237bf91774
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-03-03 23:42:40 +1300

    created the image link form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkFrom from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
 class App extends Component {
@@ -9,8 +10,8 @@ class App extends Component {
       <div className="App">
         <Navigation />
         <Logo />
-        {/*<ImageLinkFrom />
-        <FaceRecognition*/}
+        <ImageLinkFrom />
+        {/* <FaceRecognition /> */}
         
       </div>
     );
---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ImageLinkForm = () => {
+    return (
+        <div>
+            <p className='f4'>
+                {'This Magic will detect faces in your pictures. Git it a try!'}
+            </p>
+            <div>
+                <input className='f4 pa2 w-70 center' type='text' />
+                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+            </div>
+        </div>
+    );
+}
+
+export default ImageLinkForm;
