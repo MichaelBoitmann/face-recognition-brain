3eef81eea8ab8ce9a7cf811ca86a2f88cb8f4b38
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-03-06 22:35:00 +1300

    update the rank files, activate and create the imagelinkform files, and make the particlesBG works in background

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import ParticlesBg from 'particles-bg';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkFrom from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ParticlesBg type="cicle" bg={true} />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkFrom />
         {/* <FaceRecognition /> */}
         
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,7 +8,7 @@ const ImageLinkForm = () => {
                 {'This Magic will detect faces in your pictures. Git it a try!'}
             </p>
             <div className='center'>
-                <div className='form pa4 br3 shadow-5'>
+                <div className='form center pa4 br3 shadow-5'>
                     <input className='f4 pa2 w-70 center' type='text' />
                     <button className='w-30 grow f4 pill link ph3 pv2 dib white bg-light-purple'>Detect</button>
                 </div>
