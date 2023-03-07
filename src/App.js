6e08327704f0d5e86a587b98d87484aed3220f24
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-03-07 23:04:39 +1300

    added some code at imagelinkform

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ParticlesBg from 'particles-bg';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkFrom from './components/ImageLinkForm/ImageLinkForm';
@@ -8,16 +9,32 @@ import './App.css';
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onSubmit = () => {
+    console.log('click');
+  }
+
   render() {
     return (
       <div className="App">
-        <ParticlesBg num={75} type="cobweb" bg={true} opacity="0.01"/>
+        <ParticlesBg color="#0000ff" num={75} type="cobweb" bg={true} />
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkFrom />
+        <ImageLinkFrom 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit} />
         {/* <FaceRecognition /> */}
-        
       </div>
     );
       }
