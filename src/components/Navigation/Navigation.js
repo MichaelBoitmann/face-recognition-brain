96e9292668ea00bd5b82f8bb815206154d782bc3
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-03-14 23:58:07 +1300

    added the facerecognition script and css

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-const Navigation = () => {
-    return (
-        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p className='f3 link dim black underline pa3 pointer no-underline'>Sign Out</p>
-        </nav>
-    );
+const Navigation = ({ onRouteChange}) => {
+  return (
+    <div>
+      <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+        <p onClick={() => onRouteChange('home')} className='f3 link dim black underline pa3 pointer no-underline'>Sign Out
+        </p>
+      </nav>
+      <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+        <p onClick={() => onRouteChange('home')} className='f3 link dim black underline pa3 pointer no-underline'>Sign Out
+        </p>
+      </nav>
+      <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+        <p onClick={() => onRouteChange('home')} className='f3 link dim black underline pa3 pointer no-underline'>Sign Out
+        </p>
+      </nav>
+    </div>
+  );
 }
 
 export default Navigation;
