def4f3efc3c4d19ad02f2151b95487a2075481dd
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-03-22 23:07:41 +1300

    corrected the rank.js, navigation.js and signin.js files

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-
-const Rank = () => {
+const Rank = ({ name, entries}) => {
     return (
         <div>
             <div className='white f3'>
-                {'Michael, your current rank is...'}
+                {`${name}, your current rank is...`}
             </div>
             <div className='white f1'>
-                {'# 1'}
+                {entries}
             </div>
         </div>
     );
