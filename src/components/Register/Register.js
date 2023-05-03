9ca274dbd2b8ce0213ce4492e37f0fb986f48b67
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-05-03 23:01:26 +1200

    change the fetch link for rand and navigation files

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://smart-face-detector.onrender.com', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
