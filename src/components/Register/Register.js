0aabe6750788a34fbce4c7908b7e2b74918a76ab
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-05-23 00:17:19 +1200

    update register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://smart-face-detector-backend.onrender.com', {
+    fetch('https://smart-face-detector-backend.onrender.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
