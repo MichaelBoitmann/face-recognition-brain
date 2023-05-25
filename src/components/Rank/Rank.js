4f921bcfbc45cd59f534773b6baa6cf8b9eecb3b
MichaelBoitmann <michael_boitmann@yahoo.com>
2023-05-26 05:39:52 +1200

    increase font size

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,7 +4,7 @@ const Rank = ({ name, entries }) => {
     return (
         <div>
             <div className='white f3'>
-                {`${ name }, your current entry count...`}
+                <h3>{`${ name }, your current entry count...`}</h3>                
             </div>
             <div className='white f3'> {` number of entries: , ${ entries }`} 
             </div>
